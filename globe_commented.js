a19e37147b1b1e1ff8331aad9628af69aff67d4c
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-03-27 11:36:19 +0100

    refacto

--- globe_commented.js
+++ globe_commented.js
@@ -2,7 +2,7 @@
 
 const urlAPIIss = "https://api.wheretheiss.at/v1/satellites/25544";
 const urlAPINominatim = "https://nominatim.openstreetmap.org";
-const markerSvg = "<img src='image/iss.svg'></img>";
+const markerSvg = "<img src='image/iss.svg'></img>"; //image ISS sur la terre
 const date = document.querySelector("#Date");
 const latitude = document.querySelector("#Latitude");
 const longitude = document.querySelector("#Longitude");
@@ -10,8 +10,9 @@ const country = document.querySelector("#Country");
 const altitude = document.querySelector("#Altitude");
 const speed = document.querySelector("#Speed");
 const visibility = document.querySelector("#Visibility");
-const issData = [{
-    date: new Date().toLocaleDateString("fr"),
+//initialise un tableau d'objet qu'on utilise pour afficher les données de l'API de l'ISS, la bibliothèque nous oblige à utiliser un tableau
+const issData = [{ 
+    date: new Date().toLocaleDateString("fr"), //affiche la date locale format jj/mm/aaaa
     lat: "Chargement...",
     lng: "Chargement...",
     country: "Chargement...",
@@ -19,12 +20,13 @@ const issData = [{
     velocity: "Chargement...",
     visibility: "Chargement...",
 }];
+//créer un globe grâce à la bibliothèque Globe.gl : basée sur Three JS
 const globe = new Globe(document.querySelector('#globe'))
-    .globeImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-blue-marble.jpg')
-    .bumpImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-topology.png')
-    .backgroundImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png')
-    .htmlElementsData(issData)
-    .htmlElement( () => {
+    .globeImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-blue-marble.jpg') //carte
+    .bumpImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-topology.png') //relief
+    .backgroundImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png') //fond ciel étoilé
+    .htmlElementsData(issData) //on récupère les données de l'iss pour les associer à élément
+    .htmlElement( () => { //création élément ISS autour de la terre
         const element = document.createElement('div');
         element.innerHTML = markerSvg;
         element.style.width = "60px";
@@ -36,6 +38,7 @@ globe.controls().autoRotateSpeed = 0.3;
 
 // init functions
 
+//envoie les données de la position de l'ISS à la bibliothèque
 function updateISSPosition() {
     globe.htmlLat(issData.lat);
     globe.htmlLng(issData.lng);
@@ -46,8 +49,9 @@ async function getISSPosition() {
         const response = await fetch(urlAPIIss);
         const data = await response.json();
 
+        //on récupère les valeurs renvoyées par l'API pour les mettre dans notre objet
         issData[0].lat = data.latitude.toFixed(3);
-        issData[0].lng = data.longitude.toFixed(3);
+        issData[0].lng = data.longitude.toFixed(3); //toFixed : limite le nombre de décimales
         issData[0].altitude = data.altitude.toFixed(0);
         issData[0].velocity = data.velocity.toFixed(0);
         issData[0].visibility = data.visibility;
@@ -59,23 +63,26 @@ async function getISSPosition() {
     }
 }
 
+//on récupère le pays  au dessus duquel passe l'ISS grâce à l'API Nominatim avec Longitude et Latitude ("reverse geocoding")
 async function getCountryName() {
     try {
+        //on va chercher le pays avec des paramètres de recherche : longitude latitude
         const response = await fetch(`${urlAPINominatim}/reverse?format=json&lat=${issData[0].lat}&lon=${issData[0].lng}`);
         const data = await response.json();
 
-        if (data.error) {
+        if (data.error) { //si la propriété error existe dans data alors on est dans "Océan"
             issData[0].country = "Océan";
         }
         else {
-            issData[0].country = data.address.country;
+            issData[0].country = data.address.country; //j'ai la donnée et j'ai trouvé le pays
         }
     }
-    catch (error) {
+    catch (error) { //problèmes API, time out, réseau...
         console.error(error);
     }
 }
 
+//permet d'afficher jour ou nuit en fonction de la visibilité de l'ISS
 function dayOrNight(visibility) {
     if (visibility === "daylight") {
         return "Jour";
@@ -83,6 +90,7 @@ function dayOrNight(visibility) {
     return "Nuit";
 }
 
+//Récupère les informations de l'ISS pour les afficher dans le Dashboard
 function showDataUI() {
     date.innerText = issData[0].date;
     latitude.innerText = issData[0].lat;
@@ -93,6 +101,7 @@ function showDataUI() {
     visibility.innerText = dayOrNight(issData[0].visibility);
 }
 
+//Initialiser l'application, la position  de l'ISS et afficher le Dashboard
 function initiate() {
     getISSPosition();
     date.innerText = issData[0].date;
@@ -107,6 +116,7 @@ function initiate() {
 
 // execute code
 
+// on initialise l'affichage puis on lance l'intervalle toutes les 3 sec
 initiate();
 let intervalId = setInterval(() => {
     getISSPosition();
